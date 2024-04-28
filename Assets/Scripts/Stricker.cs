381c6f2d24ab8217badf912752c6d8f51a3002d8
GrigoryEbl <yatrahal.rus@mail.ru>
2024-04-28 19:19:33 +0400

    Ragdoll handler, anim stand up

---
+++ Assets/Scripts/Stricker.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stricker : MonoBehaviour
+{
+    [SerializeField, Range(0, 1000)] private float _force;
+
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                IDamageable damageable = hit.collider.GetComponentInParent<IDamageable>();
+
+                if (damageable != null)
+                {
+                    Vector3 forceDirection = (hit.point - _camera.transform.position).normalized; 
+                    forceDirection.y = 0;
+
+                    damageable.TakeDamage(forceDirection *_force, hit.point);
+
+                }
+            }
+        }
+    }
+}
