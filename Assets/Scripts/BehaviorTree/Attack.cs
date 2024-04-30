bb255887b4c7c5b7125a473c2dc201575bd9c62c
Vladislav <GimliVlad@list.ru>
2024-04-30 11:38:57 +0300

    Add attack action

---
+++ Assets/Scripts/BehaviorTree/Attack.cs
@@ -0,0 +1,20 @@
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+public class Attack : Action
+{
+    [SerializeField] private SharedUnit _unit;
+    [SerializeField] private SharedTransform _target;
+
+    public override TaskStatus OnUpdate()
+    {
+        if (_target.Value.TryGetComponent(out Unit unit))
+        {
+            _unit.Value.Attack(unit);
+            return TaskStatus.Success;
+        }
+
+        return TaskStatus.Failure;
+    }
+}
