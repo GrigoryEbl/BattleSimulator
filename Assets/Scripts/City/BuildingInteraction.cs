7e626c6681a05cc581f29369dc4ac8e4648c8074
Vladislav <GimliVlad@list.ru>
2024-05-14 11:44:22 +0300

    Add city folder

---
+++ Assets/Scripts/City/BuildingInteraction.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class BuildingInteraction : MonoBehaviour
+{
+    [SerializeField] private Transform _building;
+    [SerializeField] private Transform _lockedBuilding;
+    [SerializeField] private int _price;
+
+    private BoxCollider _triggerCollider;
+
+    public int Price => _price;
+
+    private void Awake()
+    {
+        _triggerCollider = GetComponent<BoxCollider>();
+    }
+
+    public void Unlock()
+    {
+        _lockedBuilding.gameObject.SetActive(false);
+        _building.gameObject.SetActive(true);
+        _triggerCollider.enabled = false;
+    }
+}
