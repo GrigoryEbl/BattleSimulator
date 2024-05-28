53c1a7a2819300e64400ffb01787383de9b3c332
GrigoryEbl <yatrahal.rus@mail.ru>
2024-05-28 18:38:52 +0400

    Add player money saver (city)

---
+++ Assets/Scripts/PlayerWalletSaver.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YG;
+
+public class PlayerWalletSaver : Wallet
+{
+    private Wallet _wallet;
+
+    private void Awake()
+    {
+        _wallet = GetComponent<Wallet>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _wallet.AddMoney(150);
+        }
+    }
+
+    private void OnEnable()
+    {
+        YandexGame.GetDataEvent += GetLoad;
+        _wallet.MoneyChanged += ChangeMoney;
+    }
+
+    private void OnDisable()
+    {
+        YandexGame.GetDataEvent -= GetLoad;
+        _wallet.MoneyChanged -= ChangeMoney;
+    }
+
+    private void ChangeMoney(int value)
+    {
+        Save();
+    }
+
+    private void Save()
+    {
+        YandexGame.savesData.PlayerMoney = _wallet.Money;
+    }
+
+    private void GetLoad()
+    {
+       _wallet.Initialize(YandexGame.savesData.PlayerMoney);
+    }
+}
