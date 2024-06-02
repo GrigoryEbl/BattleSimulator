9253d16cad9f1ae9ac19dee20e4c7107fdac4644
GrigoryEbl <yatrahal.rus@mail.ru>
2024-06-02 16:44:00 +0400

    Add new scin player

---
+++ Assets/Scripts/AnimTest/PlayerAnimatorController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatorController : AnimatorController
+{
+    private Movement _movement;
+    private AnimatorController _animatorController;
+
+    private void Awake()
+    {
+        _movement = GetComponent<Movement>();
+        _animatorController = GetComponent<AnimatorController>();
+    }
+
+    private void OnEnable()
+    {
+        _movement.Moving += SetState;
+    }
+
+    private void OnDisable()
+    {
+
+        _movement.Moving -= SetState;
+    }
+
+    private void SetState(bool isMoving)
+    {
+        if (!isMoving)
+        {
+            _animatorController. SetState(UnitAnimatorStates.idle);
+        }
+        else
+        {
+            _animatorController. SetState(UnitAnimatorStates.run);
+        }
+
+    }
+}
