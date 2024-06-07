205187b2204bf9e5e3eb39fce7fffabccf7ad9ac
Vladislav <GimliVlad@list.ru>
2024-06-07 08:08:18 +0300

    Add save volume

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using YG;
+
+public class Menu : MonoBehaviour
+{
+    private readonly float _pauseTimeScale = 0f;
+    private readonly float _playTimeScale = 1f;
+    private readonly float _minVolume = 0f;
+
+    [SerializeField] private FocusTracker _focusTracker;
+
+    private void Start()
+    {
+        SceneManager.LoadScene((int)SceneNames.Mines, LoadSceneMode.Additive);
+    }
+
+    public void OpenPanel(GameObject panel) => panel.SetActive(true);
+
+    public void ClosePanel(GameObject panel) => panel.SetActive(false);
+
+    public void ContinueTime()
+    {
+        Time.timeScale = _playTimeScale;
+        _focusTracker.SetCurrentTimeScale(_playTimeScale);
+    }
+
+    public void StopTime()
+    {
+        Time.timeScale = _pauseTimeScale;
+        _focusTracker.SetCurrentTimeScale(_pauseTimeScale);
+    }
+
+    public void ContinueMusic()
+    {
+        var volume = YandexGame.savesData.Volume;
+
+        AudioListener.volume = volume;
+        _focusTracker.SetCurrentVolume(volume);
+    }
+
+    public void StopMusic()
+    {
+        AudioListener.volume = _minVolume;
+        _focusTracker.SetCurrentVolume(_minVolume);
+    }
+}
